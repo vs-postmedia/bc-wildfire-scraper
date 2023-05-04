7587ee54292e454fa4e9e049496c4f1a967baa30
Nathan Griffiths <ngriffiths@postmedia.com>
2023-05-03 21:39:13 -0700

    prep for fires of note script

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const fetchCurrentFires = require('./scripts/fetch-current-fires');
 
 // VARS
 const data_dir = 'data';
+const wildfire_rss_feed = 'http://bcfireinfo.for.gov.bc.ca/FTP/!Project/WildfireNews/xml/All-WildfireNews.xml';
 
 async function init() {
+	// get RSS feed for fires of note
+	// const fon_ids = await parseRssFeed(wildfire_rss_feed);
+
 	// get current fires from BC data
 	fetchCurrentFires(data_dir);
-	
-	// get latest firesmoke data
 
+	// get latest firesmoke data
 }
 
 init();
